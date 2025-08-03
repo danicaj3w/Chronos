eb606b415fbdc6102473d807012193816fa3946b
TomDomBom <59707328+TomDomBom@users.noreply.github.com>
2025-08-02 17:31:22 -0700

    initial push
    template + timebody and playercontroller adjustment
    
    deleted some boilerplate for the project.

---
+++ My project/Assets/Scripts/Mechanics/PlayerController.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+using Platformer.Model;
+using Platformer.Core;
+using UnityEngine.InputSystem;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// This is the main class used to implement control of the player.
+    /// It is a superset of the AnimationController class, but is inlined to allow for any kind of customisation.
+    /// </summary>
+    public class PlayerController : KinematicObject
+    {
+        public AudioClip jumpAudio;
+        public AudioClip respawnAudio;
+        public AudioClip ouchAudio;
+
+        /// <summary>
+        /// Max horizontal speed of the player.
+        /// </summary>
+        public float maxSpeed = 7;
+        /// <summary>
+        /// Initial jump velocity at the start of a jump.
+        /// </summary>
+        public float jumpTakeOffSpeed = 7;
+
+        public JumpState jumpState = JumpState.Grounded;
+        private bool stopJump;
+        /*internal new*/
+        public Collider2D collider2d;
+        /*internal new*/
+        public AudioSource audioSource;
+        public Health health;
+        public bool controlEnabled = true;
+
+        bool jump;
+        Vector2 move;
+        SpriteRenderer spriteRenderer;
+        internal Animator animator;
+        readonly PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        private InputAction m_MoveAction;
+        private InputAction m_JumpAction;
+
+        public Bounds Bounds => collider2d.bounds;
+
+        private TimeBody timeBody;
+        private InputAction m_RewindAction;
+        public bool isRewinding = false;
+
+        void Awake()
+        {
+            health = GetComponent<Health>();
+            audioSource = GetComponent<AudioSource>();
+            collider2d = GetComponent<Collider2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
+
+            m_MoveAction = InputSystem.actions.FindAction("Player/Move");
+            m_JumpAction = InputSystem.actions.FindAction("Player/Jump");
+
+            m_MoveAction.Enable();
+            m_JumpAction.Enable();
+
+
+            timeBody = GetComponent<TimeBody>();
+            m_RewindAction = InputSystem.actions.FindAction("Player/Rewind"); 
+            m_RewindAction.Enable();
+        }
+
+        protected override void Update()
+        {
+            if (isRewinding) return;
+
+            if (controlEnabled)
+            {
+                move.x = m_MoveAction.ReadValue<Vector2>().x;
+                if (jumpState == JumpState.Grounded && m_JumpAction.WasPressedThisFrame())
+                    jumpState = JumpState.PrepareToJump;
+                else if (m_JumpAction.WasReleasedThisFrame())
+                {
+                    stopJump = true;
+                    Schedule<PlayerStopJump>().player = this;
+                }
+            }
+            else
+            {
+                move.x = 0;
+            }
+            UpdateJumpState();
+            base.Update();
+        }
+
+        void UpdateJumpState()
+        {
+            jump = false;
+            switch (jumpState)
+            {
+                case JumpState.PrepareToJump:
+                    jumpState = JumpState.Jumping;
+                    jump = true;
+                    stopJump = false;
+                    break;
+                case JumpState.Jumping:
+                    if (!IsGrounded)
+                    {
+                        Schedule<PlayerJumped>().player = this;
+                        jumpState = JumpState.InFlight;
+                    }
+                    break;
+                case JumpState.InFlight:
+                    if (IsGrounded)
+                    {
+                        Schedule<PlayerLanded>().player = this;
+                        jumpState = JumpState.Landed;
+                    }
+                    break;
+                case JumpState.Landed:
+                    jumpState = JumpState.Grounded;
+                    break;
+            }
+        }
+        protected override void OnEnable()
+        {
+            // When this object is enabled, subscribe our methods to the input action events.
+            m_RewindAction.performed += OnRewindStarted;
+            m_RewindAction.canceled += OnRewindCanceled;
+            base.OnEnable();
+        }
+
+        protected override void OnDisable()
+        {
+            // It's crucial to unsubscribe when the object is disabled to prevent memory leaks.
+            m_RewindAction.performed -= OnRewindStarted;
+            m_RewindAction.canceled -= OnRewindCanceled;
+            base.OnDisable();
+        }
+        protected override void ComputeVelocity()
+        {
+            if (jump && IsGrounded)
+            {
+                velocity.y = jumpTakeOffSpeed * model.jumpModifier;
+                jump = false;
+            }
+            else if (stopJump)
+            {
+                stopJump = false;
+                if (velocity.y > 0)
+                {
+                    velocity.y = velocity.y * model.jumpDeceleration;
+                }
+            }
+
+            if (move.x > 0.01f)
+                spriteRenderer.flipX = false;
+            else if (move.x < -0.01f)
+                spriteRenderer.flipX = true;
+
+            animator.SetBool("grounded", IsGrounded);
+            animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+            targetVelocity = move * maxSpeed;
+        }
+
+        public enum JumpState
+        {
+            Grounded,
+            PrepareToJump,
+            Jumping,
+            InFlight,
+            Landed
+        }
+        // This function is called when the Rewind action is performed (key pressed)
+        private void OnRewindStarted(InputAction.CallbackContext context)
+        {
+
+            if (!isRewinding)
+            {
+                isRewinding = true;
+                timeBody.StartRewind();
+            }
+        }
+
+        // This function is called when the Rewind action is canceled (key released)
+        private void OnRewindCanceled(InputAction.CallbackContext context)
+        {
+            if (isRewinding)
+            {
+                isRewinding = false;
+                timeBody.StopRewind();
+            }
+        }
+    }
+}
